4450b4080b72b57eea57504bbfea656b097a98a3
ethansein <112203256+ethansein@users.noreply.github.com>
2022-09-14 14:38:55 -0500

    Updated

--- logs.py
+++ logs.py
@@ -2,6 +2,7 @@
 #
 # Outputs the value of g(x) = ln(100-x^2) + sqrt(84 - 5x - x^2) at the input x-value.
 
+from lib2to3.pgen2.token import LESSEQUAL
 import math
 
 def g(x):
@@ -10,4 +11,12 @@ def g(x):
 x = input("Input the value for x: ")
 x = float(x)
 
-print("g(" + str(x) + ") = " + str(g(x)))
+
+if not x > -10:
+    print("Invalid input, number is too low")
+
+if not x <= 7:
+    print("Invalid input, number is too high")
+
+else: 
+    print("g(" + str(x) + ") = " + str(g(x)))
